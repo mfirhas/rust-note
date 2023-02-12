a7771bb120696c330dd2ad097aea6c162f236916
mfathirirhas <mfathirirhas@gmail.com>
2023-02-13 06:59:20 +0700

    Add generic method example

--- examples/basics_generic_12/src/main.rs
+++ examples/basics_generic_12/src/main.rs
@@ -4,46 +4,40 @@ fn main() {
     print::<i32>(123);
 
     let r = ret::<i32>(1);
-    println!("{:?}",r);
+    println!("{:?}", r);
     let r = ret::<f32>(1_f32);
-    println!("{:?}",r);
+    println!("{:?}", r);
     let r = ret(123);
-    println!("{:?}",r);
+    println!("{:?}", r);
     let r: f32 = ret(123_f32);
-    println!("{:?}",r);
+    println!("{:?}", r);
 
-    let a = A {
-        a: 123,
-        b: 34,
-    };
+    let a = A { a: 123, b: 34 };
 
-    let a = A::<f32> {
-        a: 123.2,
-        b: 23.0,
-    };
+    let a = A::<f32> { a: 123.2, b: 23.0 };
 
-    let a2 = A2::<i32,f32> {
-        a: 123,
-        b: 34.4,
-    };
+    let a2 = A2::<i32, f32> { a: 123, b: 34.4 };
 
-    let a2 = A2 {
-        a: 123,
-        b: 34.5,
-    };
+    let a2 = A2 { a: 123, b: 34.5 };
 
     let b = B::<i32>::Field2(123);
-    println!("{:?}",b);
+    println!("{:?}", b);
 
     let b2 = B2::<i32, f32>::Field2(123);
     let b2: B2<&str, i32> = B2::Field2("123");
 
     // let d: D<i32> = 123;
     // d::method1(123);
-    let a = A::<i32> {
-        a: 123,
-        b: 234,
-    };
+    // <i32 as D>::method1(123);
+    // <i32 as D<T>>::method1(123);
+    // D::<i32>::method1(123);
+    // <i32 as D>::method1(123);
+    i32::method1(123);
+    i32::method2();
+    let sdf: i32 = i32::method3();
+    println!("==> {}", i32::method3());
+
+    let a = A::<i32> { a: 123, b: 234 };
     a.method2(123);
     let a = A::<i32>::method1(123);
 
@@ -54,11 +48,29 @@ fn main() {
         b: String::from("test"),
     };
     dbg!(ms.method1());
+
+    let a: String = roll();
+    println!("--> {a}", )
+}
+
+pub trait Rollable {
+    /// Roll the die
+    fn roll() -> Self;
+}
+
+pub fn roll<T: Rollable>() -> T {
+    Rollable::roll() // <- Note that here `Rollable` is the current type for a given call!
+}
+
+impl Rollable for String {
+    fn roll() -> Self {
+        String::from("rolled")
+    }
 }
 
 pub trait MyTrait {
     fn method1(&self) -> i32;
-    fn method2(param1: &str) -> Result<(),String>;
+    fn method2(param1: &str) -> Result<(), String>;
     fn method3(param1: String);
 }
 
@@ -72,7 +84,7 @@ impl MyTrait for MyStruct {
         self.a
     }
 
-    fn method2(param1: &str) -> Result<(),String> {
+    fn method2(param1: &str) -> Result<(), String> {
         if param1.is_empty() {
             return Err("empty string".to_string());
         }
@@ -88,11 +100,21 @@ impl MyTrait for MyStruct {
 
 trait D<T> {
     fn method1(param: T);
+    fn method2();
+    fn method3() -> T;
 }
 
 impl D<i32> for i32 {
     fn method1(param: i32) {
-        print!("{param}");
+        print!("ngenuu mas blabla {param}");
+    }
+
+    fn method2() {
+        println!("yeeee")
+    }
+
+    fn method3() -> i32 {
+        123
     }
 }
 
@@ -100,22 +122,18 @@ impl D<i32> for i32 {
 enum B<T> {
     Field1,
     Field2(T),
-    Field3 {
-        t: T,
-    }
+    Field3 { t: T },
 }
 
 #[derive(Debug)]
 enum B2<T, E> {
     Field1,
     Field2(T),
-    Field3 {
-        t: E,
-    }
+    Field3 { t: E },
 }
 
 fn print<T: Debug + Display>(param: T) {
-    println!("{:?}",param);
+    println!("{:?}", param);
 }
 
 struct A<T> {
